41165bedee45a7e608dbe1980be2ec89dfac9dee
agungtu <agungtriutomo85@gmail.com>
2026-05-06 13:34:23 +0700

    aa

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
+import sys
+import os
+
+# Tambahkan root project ke path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+# Tambahkan folder Django
+sys.path.append(os.path.join(os.path.dirname(__file__), "djangowebite2026"))
+
 from djangowebite2026.wsgi import application
 
 app = application
